c932878ebc26512ca79eb5ecfd0dbd7d4fd6b58f
aarush73210-ai <aarush73210@gmail.com>
2026-06-16 17:50:59 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+import { useEffect, useState } from 'react'
 
+const supabase = createClient(
+  'https://pfbigsjrgcizycvidfyx.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBmYmlnc2pyZ2NpenljdmlkZnl4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ3
